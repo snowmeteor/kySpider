f068dca870a3e622c160604bf40c322a714b270b
snowmeteor <snowmeteor@163.com>
2019-11-04 10:05:48 +0800

    用正则获取script标签中内容

--- spider/src/test/java/org/ky/spider/Demo.java
+++ spider/src/test/java/org/ky/spider/Demo.java
@@ -4,9 +4,13 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// 测试头条新闻兼容性
-		String url = "https://www.toutiao.com/i6754183669176336909/";
+		String url = "https://www.toutiao.com/a6755070347285889543/";
 
-		System.out.println(CrawlHelper.getDynamicPageHtml(url)); 
+		String html = CrawlHelper.getStaticPageHtml(url);
+		//System.out.println(html);
+
+		//用正则获取需要的内容
+		System.out.println(CrawlHelper.fetchTextByRegex(html, "articleInfo(.*?)commentInfo:"));
 	}
 
 }
